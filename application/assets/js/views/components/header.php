20a7e3c343c5dbe7858316fff4996662ab4a35b1
seif <smaachaoui@outlook.com>
2025-10-02 21:06:03 +0200

    Ajout du chemin absolu  vers le fichier style.css

--- application/assets/js/views/components/header.php
+++ application/assets/js/views/components/header.php
@@ -1,4 +1,12 @@
+<link rel="stylesheet" href="/application/assets/css/style.css">
+
+
 <header>
+
+    <div class ="logo">
+        <img class="logo_picture" src="/application/assets/img/logo.png" alt="Un logo personnalisé">
+    </div>
+
     <nav>
         <a href="./index.php">Accueil</a>
         <a href="./covoiturages.php">Covoiturages</a>
