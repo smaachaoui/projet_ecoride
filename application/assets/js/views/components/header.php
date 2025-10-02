a89acc872cfa69c371ac07f61754b87ac4fe209b
seif <smaachaoui@outlook.com>
2025-10-02 10:15:48 +0200

    Mise en place du header

---
+++ application/assets/js/views/components/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <a href="./pages/index.php">Accueil</a>
+        <a href="./pages/covoiturages.php">Covoiturages</a>
+        <a href="./pages/contact.php">Contact</a>
+        <a href="./pages/login.php">Connexion</a>
+        <a href="./pages/register.php">Inscription</a>
+    </nav>
+</header>
