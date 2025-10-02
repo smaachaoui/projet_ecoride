b4abb7ec603b978ada788e08fedc4fa91aadfb2e
seif <smaachaoui@outlook.com>
2025-10-02 11:23:05 +0200

    Correction des chemins dans les attributs href de mes liens de navigation

--- application/assets/js/views/components/header.php
+++ application/assets/js/views/components/header.php
@@ -1,9 +1,9 @@
 <header>
     <nav>
-        <a href="./pages/index.php">Accueil</a>
-        <a href="./pages/covoiturages.php">Covoiturages</a>
-        <a href="./pages/contact.php">Contact</a>
-        <a href="./pages/login.php">Connexion</a>
-        <a href="./pages/register.php">Inscription</a>
+        <a href="./index.php">Accueil</a>
+        <a href="./covoiturages.php">Covoiturages</a>
+        <a href="./contact.php">Contact</a>
+        <a href="./login.php">Connexion</a>
+        <a href="./register.php">Inscription</a>
     </nav>
 </header>
