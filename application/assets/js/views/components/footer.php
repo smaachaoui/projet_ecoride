8370d9166cb4f5aff8e196d0e880aeb5d22ea61f
seif <smaachaoui@outlook.com>
2025-10-02 21:06:43 +0200

    Ajout du chemin absolu qui pointe vers la feuille de style

--- application/assets/js/views/components/footer.php
+++ application/assets/js/views/components/footer.php
@@ -1,5 +1,6 @@
+<link rel="stylesheet" href="/application/assets/css/style.css">
 <footer>
-    <div>
+    <div class="links">
         <a href="mailto:contact@ecoride.com">contact@ecoride.com</a>
         <a href="#">Mentions légales</a>
     </div>
