de38e6ae9655e8ead482612f5fe4dd91dfb40133
seif <smaachaoui@outlook.com>
2025-10-02 09:40:25 +0200

    Renommage des fichiers html en .php dans le sous-dossier components et pages du dossier views

---
+++ application/assets/js/views/pages/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css">
+        <script type="module" src="./js/main.js"></script>
+        <title>EcoRide - Accueil</title>
+    </head>
+    
+    <body>
+        
+        <?php include 'header.html'; ?>
+
+        <main>
+            <section>
+        
+                <h1>Bienvenue sur EcoRide</h1>
+                <p>Une conduite écologique et économique</p>
+                
+            </section>
+
+            <section>
+                <h2> Tu cherches un chauffeur ?</h2>
+            </section>
+
+            <section>
+                <h3>Pourquoi choisir EcoRide ?</h3>
+            </section>
+
+        </main>
+
+        <?php include 'footer.html'; ?>
+      
+    </body>
+</html>
